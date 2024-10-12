a99f651afe62b942d673834030c05e1d5b9a3987
Sailious <1097561282@qq.com>
2024-10-13 00:33:03 +0800

    Test the database connection and complete the new API methods for POST, PUT, DELETE, and ensure they are thoroughly tested.

--- controllerAPI/MySQL-Database/A3-database.sql
+++ controllerAPI/MySQL-Database/A3-database.sql
@@ -5,7 +5,7 @@ USE `crowdfunding_db`;
 -- Host: localhost    Database: crowdfunding_db
 -- ------------------------------------------------------
 -- Server version	8.0.39
-
+CATEGORY_ID
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
